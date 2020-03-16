b3cbdcdddcc541e02d4999f212b9577bb82a0c5a
AnhQuan Nguyen <j4qfrost@gmail.com>
2020-03-16 04:16:32 -0700

    image loading works

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,10 @@ extern crate sdl2;
 #[macro_use]
 extern crate log;
 
-// use crate::sdl2::image::{InitFlag, LoadTexture};
 use std::env;
 use std::path::Path;
 use std::time::Instant;
-// use sdl2::image::{InitFlag, LoadTexture};
+use sdl2::image::{InitFlag, LoadTexture};
 
 mod util;
 use crate::util::frame_rate_sleep;
@@ -18,7 +17,7 @@ use crate::util::EventResult;
 
 fn main() {
     let sdl = sdl2::init().unwrap();
-    // let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG).unwrap();
+    let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG).unwrap();
     let video_subsystem = sdl.video().unwrap();
     let display_mode = video_subsystem.current_display_mode(0).unwrap();
     let (d_width, d_height) = (display_mode.w as u32, display_mode.h as u32);
@@ -42,27 +41,27 @@ fn main() {
     let refresh_rate = neovide::settings::SETTINGS
         .get::<neovide::window::WindowSettings>()
         .refresh_rate as f32;
-    // let path = env::current_dir().unwrap();
-    // println!("The current directory is {}", path.display());
-    // let mut canvas = main_window
-    //     .into_canvas()
-    //     .software()
-    //     .build()
-    //     .map_err(|e| e.to_string())
-    //     .unwrap();
-    // let png = Path::join(&path, "assets/rust.png");
-    // println!("{:?}", png);
+    let path = env::current_dir().unwrap();
+    println!("The current directory is {}", path.display());
+    let mut canvas = main_window
+        .into_canvas()
+        .software()
+        .build()
+        .map_err(|e| e.to_string())
+        .unwrap();
+    let png = Path::join(&path, "assets/rust.png");
+    println!("{:?}", png);
 
-    // let texture_creator = canvas.texture_creator();
-    // let texture = match texture_creator.load_texture("assets/rust.png") {
-    //     Ok(t) => t,
-    //     Err(e) => panic!("{:?}", e),
-    // };
+    let texture_creator = canvas.texture_creator();
+    let texture = match texture_creator.load_texture("assets/rust.png") {
+        Ok(t) => t,
+        Err(e) => panic!("{:?}", e),
+    };
 
-    // canvas.copy(&texture, None, None);
-    // canvas.present();
+    canvas.copy(&texture, None, None);
+    canvas.present();
 
-    // main_window = canvas.into_window();
+    main_window = canvas.into_window();
 
     'running: loop {
         let frame_start = Instant::now();
